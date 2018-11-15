b9956ca8bbca3fa73b34eaca52c64c328d7f2836
wuhongbo <wuhongbo2014@xiaochuankeji.cn>
2018-11-15 11:39:56 +0800

    11:38组修改，不提交，只提交外部工程，看看看会怎么样

--- foundation/src/main/java/com/test/wu/foundation/Plus.java
+++ foundation/src/main/java/com/test/wu/foundation/Plus.java
@@ -9,6 +9,7 @@ public class Plus {
     public static int plusTheNum(){
         int a = 1105;
         int b = 1111;
+        int c = 1138;
         return  1811151044;        //10月44日10点16分做修改
     }
 }
